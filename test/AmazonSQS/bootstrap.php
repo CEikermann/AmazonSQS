5dd81ddfbc6ed8b85b6d7d1f8dcb98d5795563b2
Christian Eikermann <christian@chrisdev.de>
2012-04-02 22:35:48 +0200

    Added composer support

--- test/AmazonSQS/bootstrap.php
+++ test/AmazonSQS/bootstrap.php
@@ -9,19 +9,4 @@
  * file that was distributed with this source code.
  */
 
-date_default_timezone_set('UTC'); 
-
-require_once __DIR__.'/../../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->useIncludePath(true);
-
-$loader->registerNamespaces(array(
-    'AmazonSQS' => __DIR__.'/../../src',
-    'Symfony'   => __DIR__.'/../../vendor/',
-    'apiTalk'   => __DIR__.'/../../vendor/apiTalk/src',
-));
-
-$loader->register();
+require_once __DIR__.'/../../vendor/.composer/autoload.php';
